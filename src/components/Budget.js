36ab193e4aad1dc154e894aaa59eef033c53b438
oartigue <33022163+oartigue@users.noreply.github.com>
2023-05-26 18:03:58 +0200

    Input for budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,32 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    const { dispatch } = useContext(AppContext);
+
+    //const [cost, setCost] = useState('');
+
+    const setBudget = (value) => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: value
+        });
+    };
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span>
+            <input
+                required='required'
+                type='number'
+                id='budget'
+                step="10"
+                value={budget}
+                style={{ marginLeft: '2rem' , size: 10}}
+                onChange={(event) => setBudget(event.target.value)}>
+                </input>
         </div>
     );
 };
 export default Budget;
-
