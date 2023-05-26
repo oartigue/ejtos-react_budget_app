0c48b4e8a4a939a5a2c38a6edc41631895d48785
oartigue <33022163+oartigue@users.noreply.github.com>
2023-05-26 17:30:15 +0200

    add descrease

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
+import { FaTimesCircle } from 'react-icons/fa';
+import { IoAddCircle, IoRemoveCircle } from "react-icons/io5"
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
@@ -25,12 +26,25 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
     return (
         <tr>
         <td>{props.name}</td>
         <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        <td><IoAddCircle size='2.2em' color="green" onClick={event=> increaseAllocation(props.name)}>+</IoAddCircle></td>
+        <td><IoRemoveCircle size='2.2em' color="red" onClick={event=> decreaseAllocation(props.name)}>+</IoRemoveCircle></td>
+        <td><FaTimesCircle size='1.5em' onClick={handleDeleteExpense}></FaTimesCircle></td>
         </tr>
     );
 };
